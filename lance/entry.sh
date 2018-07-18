0441bfea95552402bbec5d109d2a248a9077a8fe
Daniel Baur <daniel.baur@uni-ulm.de>
2018-07-18 17:29:26 +0200

    Added etcd registry command to entry point of lance agent

--- lance/entry.sh
+++ lance/entry.sh
@@ -2,5 +2,14 @@
 
 set -x
 
+env_required() {
+  echo "EnvironmentVariable $1 is required."
+  exit 1
+}
+
+if [ -z "$ETCD_HOST" ]; then
+  env_required "ETCD_HOST"
+fi
+
 # Run the service
-java -jar lance-agent.jar
+java -Dlca.client.config.registry=etcdregistry -Dlca.client.config.registry.etcd.hosts=${ETCD_HOST} -jar lance-agent.jar
