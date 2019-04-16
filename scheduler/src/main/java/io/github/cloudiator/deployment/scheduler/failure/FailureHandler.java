ababc624e7bb793287640e5969f6623caa1e2edb
Daniel Baur <daniel.baur@uni-ulm.de>
2019-04-16 15:34:59 +0200

    logging implementation of failure handler

--- scheduler/src/main/java/io/github/cloudiator/deployment/scheduler/failure/FailureHandler.java
+++ scheduler/src/main/java/io/github/cloudiator/deployment/scheduler/failure/FailureHandler.java
@@ -3,18 +3,24 @@ package io.github.cloudiator.deployment.scheduler.failure;
 import com.google.inject.Singleton;
 import io.github.cloudiator.deployment.domain.CloudiatorProcess;
 import io.github.cloudiator.domain.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class FailureHandler implements NodeFailureReportingInterface,
     ProcessFailureReportingInterface {
 
+  private static final Logger LOGGER = LoggerFactory
+      .getLogger(FailureHandler.class);
+
 
   public void addNodeFailure(Node node) {
+    LOGGER.warn(String.format("Registering failure of node %s", node));
 
   }
 
   public void addProcessFailure(CloudiatorProcess cloudiatorProcess) {
-
+    LOGGER.warn(String.format("Registering failure of process %s", cloudiatorProcess));
   }
 
 }
